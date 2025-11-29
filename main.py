ba02edd917d999cac92401e9e212a2fe272c4026
Immortalizex <luckydev001.test@gmail.com>
2025-11-29 03:44:31 +0200

    update port

--- main.py
+++ main.py
@@ -24,6 +24,6 @@ def sora():
     return jsonify({"status": "success", "received": signedlink})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, use_reloader=False)
+    app.run(debug=True, port=80, use_reloader=False)
     
 print()
