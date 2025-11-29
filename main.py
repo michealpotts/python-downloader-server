24c7ff806d2ae72da0d5f1dbad1bf8b3e09ba8cf
Immortalizex <luckydev001.test@gmail.com>
2025-11-29 03:53:48 +0200

    change port

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from bot import find_sign_link
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-
 CORS(app)
 
 @app.route("/sora", methods=["POST","OPTIONS"])
 def sora():
-    # Parse JSON from request
     if request.method == "OPTIONS":
         response = jsonify({"status": "OK"})
         response.headers.add("Access-Control-Allow-Origin", "*")
@@ -17,13 +14,11 @@ def sora():
         response.headers.add("Access-Control-Allow-Headers", "Content-Type")
         return response, 200
     
-    data = request.get_json(force=True)  # force=True ignores content type
+    data = request.get_json(force=True)
     if not data:
         return jsonify({"error": "No data received"}), 400
     signedlink = find_sign_link(data['url'])
     return jsonify({"status": "success", "received": signedlink})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=80, use_reloader=False)
-    
-print()
+    app.run(debug=True, host='0.0.0.0', port=80, use_reloader=False)
